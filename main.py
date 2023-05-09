af5c080407f6c90ad7d2ae318e908f8d9fa4fb59
Diego Racero <dracero@fi.uba.ar>
2023-05-09 10:11:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import openai
+from decouple import config
 
+openai.api_key = config('KEY')
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+model_engine = "gpt-3.5-turbo"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/generate")
+async def generate_text(prompt: str):
+    completions = openai.ChatCompletion.create(
+      model= model_engine,
+      temperature=.2,
+      top_p=0.3,
+      messages=[
+        {"role": "user", "content": prompt}
+      ]
+    )
+    # Print the generated text
+    message = completions.choices[0]['message']['content']
+    return (message)
